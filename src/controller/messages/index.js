902e60771668dbd1ae3cc952fb62b509c2757377
anneiarriola <95490269+anneiarriola@users.noreply.github.com>
2023-10-10 13:16:45 -0500

    Add user ifno to socket

--- src/controller/messages/index.js
+++ src/controller/messages/index.js
@@ -1,10 +1,15 @@
 const Message = require('../../models/message');
+const User = require('../../models/user');
 
 exports.createMessage = async(req, res) => {
-  const { user_sender_id, chat_room_id ,content,user_name } = req.body;
+  const { user_sender_id, chat_room_id ,content } = req.body;
   try {
-    const newMessage = new Message({ user_sender_id,chat_room_id, content, user_name });
+    const newMessage = new Message();
+    newMessage.user_sender_id = user_sender_id;
+    newMessage.chat_room_id = chat_room_id;
+    newMessage.content = content;
     const message = await newMessage.save();
+    message.user_sender_id = await User.findById(user_sender_id)
     require('../../socket').onMessage(chat_room_id,message)
     res.json(newMessage);
   } catch (error) {
@@ -16,7 +21,10 @@ exports.createMessage = async(req, res) => {
 
 exports.getAllMessages = async(req,res) => {
   try {
-    const messages = await Message.find().sort({ timestamp: 1 });
+    const { chat_room_id } = req.query;
+    const messages = await Message.find({ 
+      chat_room_id: chat_room_id 
+    }).populate('user_sender_id').sort({ timestamp: 1 }).exec();
     res.json(messages);
   } catch (error) {
     console.error(error);
